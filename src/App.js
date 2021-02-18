c429fdb35e5b03a2472aeb2c2fdd6135908e3edb
Sakshi Dwivedi <sakshithedwivedi@gmail.com>
2021-02-18 20:28:01 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import styled from "styled-components";
+import { AccountBox } from "./components/accountBox";
+import FrontPage from './FrontPage';
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 200vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <AppContainer>
+        <FrontPage />
+        <div className="form">
+          <div className="text">
+            <p>Login or Sign Up!</p>
+            <span>Design, Develop and influence. Get started now!</span>
+          </div>
+          <AccountBox />
+        </div>
+        <div className="footer">
+        © 2020-2021 Let's Code
+      </div>
+      </AppContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FrontPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './FrontPage.css';
+
+const FrontPage = () => {
+
+    return (
+        <div className="Page">
+            <h1>Let's Code.</h1>
+            <p>Learning HTML and CSS is a lot more challenging than it used to be. Responsive web design adds more layers of complexity to design and develop websites. Thereby, we present you our resources and platform to pave your way to professionalism.</p>
+            <div class="line"></div>
+            <div class="parent">
+                <div class="boxone">LEARN.</div>
+                <div class="boxtwo">CODE.</div>
+                <div class="boxthree">EXPLORE.</div>
+            </div>
+        </div>
+    )
+}
+
+export default FrontPage;
